561b8dbd210bdcf3fe5e5a411f420afc4c546975
Nozadze Otto <78607992+NozadzeOto18@users.noreply.github.com>
2021-12-06 20:31:53 +0400

    Update PalindromeActivity.kt

--- PalindromeActivity.kt
+++ PalindromeActivity.kt
@@ -12,26 +12,22 @@ class PalindromeActivity : AppCompatActivity() {
         init()
     }
 
-    // We have EditText layout and button
     private fun init() {
-        var Str = inputET.text.toString()
-
-        //reverse inputted text
-        var reversedStr = inputET.text.toString().reversed()
-
-        //With button click we compare inputted text to reversed inputted text
-        button.setOnClickListener{
-
-            //check if inputted text is empty
-            if (inputET.text.toString().isNotEmpty()){
-                if (Str == reversedStr){
-                    Toast.makeText(this, "Text is palindrome", Toast.LENGTH_SHORT).show()
-                }else{
-                    Toast.makeText(this, "Text is not palindrome", Toast.LENGTH_SHORT).show()
-                }
-            }else{
-                Toast.makeText(this, "Please input text", Toast.LENGTH_SHORT).show()
+        if (inputET.text.toString().isNotEmpty()) {
+            if (isPalindrome(inputET.text.toString())) {
+                Toast.makeText(this, "inputted text is palindrome", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "inputted text is not palindrome", Toast.LENGTH_SHORT).show()
             }
+        }else{
+            Toast.makeText(this, "please input text", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun isPalindrome(text: String): Boolean {
+        var Str = text
+        var reversedStr = Str.reversed()
+        return (Str == reversedStr)
+
     }
 }
