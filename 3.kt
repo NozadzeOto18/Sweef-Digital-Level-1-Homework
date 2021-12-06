5c8a1893f23385c61320c376122b80de79a468e3
Nozadze Otto <78607992+NozadzeOto18@users.noreply.github.com>
2021-12-06 22:24:43 +0400

    Update 3.kt

--- 3.kt
+++ 3.kt
@@ -1,5 +1,5 @@
 private fun notContain(array: Array<Int>): Int {
-        var maxNum = array.max()
+        var maxNum: Int = array.max() ?: 0
         var minNum = 0
         for (num in maxNum!! until 0 step -1) {
             println(num)
