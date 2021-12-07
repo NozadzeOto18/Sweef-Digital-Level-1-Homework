ff5aeb6f8605a424fa1399c4d5469a0430f9e727
Nozadze Otto <78607992+NozadzeOto18@users.noreply.github.com>
2021-12-07 14:27:08 +0400

    Create 5.kt

---
+++ 5.kt
@@ -0,0 +1,25 @@
+private fun init() {
+        // number of floors ( n )
+        var floor = 2
+        countVariants(floor)
+    }
+
+    fun factorial(num: Int): Int{
+        var factorial : Long = 1
+        for (i in 1..num) {
+            factorial *=i.toLong()
+        }
+        return factorial.toInt()
+    }
+
+    private fun countVariants(stearsCount: Int): Int{
+        //number of steps between each floor
+        var step = 5
+        var fullNumber = step * stearsCount
+        var result = 2 // first variant (if all stages will be 1 step) and second (if all stages will be 2 step) 
+        while(fullNumber > fullNumber/2){
+            result += factorial(fullNumber)
+            fullNumber--
+        }
+        return result
+    }
